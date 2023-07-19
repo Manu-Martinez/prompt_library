87cd3d3a3ba90e729c8d9678dfe960e82b162045
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-19 02:09:37 -0300

    fixed issues with Nav file

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -6,50 +6,47 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-	const isUserLoggedIn = true;
-	
-	return (
-	<nav className="flex-between w-full mb-16 pt-3">
-		<Link href="/" className="flex gap-2 flex-center">
-			<Image 
-			src="/assets/images/logo.svg" 
-			alt="Prompt Library Logo"
-			width={30}
-			height={30}
-			className="object-contain"
-			/>
-			<p className="logo_text">Prompt Library</p>
-		</Link>
+  const isUserLoggedIn = true;
 
-		{/* Responsive Navigation */}
-		<div className="sm:flex hidden">
-			{
-				isUserLoggedIn ? (
-				<div className="flex gap-3 md:gap-5">
-					<Link href="/create-prompt" className="black_btn">
-					Create Post
-					</Link>
-					<button type="button" onClick={signOut} className="outline_btn">
-						Sign Out
-					</button>
-					<Link href="/profile">
-						<image 
-						src="/assets/images/logo.svg" 
-						width={35} 
-						height={35} 
-						className="rounded-full" 
-						alt="profile">
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Prompt Library Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Prompt Library</p>
+      </Link>
 
-						</image>
-					</Link>
-				</div>
-				) : (
-					<></>
-				
-			)}
-		</div>
-	</nav>
-)
-}
+      {/* Responsive Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <image
+                src="/assets/images/logo.svg"
+                width={35}
+                height={35}
+                className="rounded-full"
+                alt="profile"
+              ></image>
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
