a8a7b975a473756860c7a4a65496b4d13d640c02
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-19 03:15:58 -0300

    added Provider to Layout file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,29 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-	title: "Prompt Library",
-	description: "A place to find and share AI prompts"
-} 
+  title: "Prompt Library",
+  description: "A place to find and share AI prompts",
+};
 
 const RootLayout = ({ children }) => {
   return (
-	<html lang='en'>
-		<body>
-			<div className='main'>
-				<div className='gradient' />
-			</div>
-			<main className='app'>
-				<Nav />
-				{children}
-			</main>
-		</body>
-	</html>
-  )
-}
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
