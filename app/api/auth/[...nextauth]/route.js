9feebb6ab1c8f1ec5f12e6433f25c13490e3015b
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-20 02:31:56 -0300

    created the session handler

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,49 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler =  NextAuth({
-	providers: [
-		GoogleProvider({
-			clientId: "",
-			clientSecret: "",
-		})
-	],
-	async session({ session }) {
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
-	},
-	async signIn({ profile }) {
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
 
-	}
-}) 
+    session.user.id = sessionUser._id.toString();
 
-export {handler as GET, handler as POST };
+    return session;
+  },
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // first, it will check if the user exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      // if it does not, it will create it
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+});
+
+export { handler as GET, handler as POST };
