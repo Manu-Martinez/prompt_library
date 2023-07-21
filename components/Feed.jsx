2260e00db61a1b2684f2072a439507293f468be4
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-21 16:55:08 -0300

    prompts and creators shown on Feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,12 +2,37 @@
 import { useState, useEffect  } from 'react'
 import PromptCard from './PromptCard'
 
+const PromptCardList = ({ data, handleTagClick }) => {
+	return (
+		<div className='mt-16 prompt_layout'>
+			{data.map((post) => (
+				<PromptCard key={post._id}
+				post={post}
+				handleTagClick={handleTagClick}
+				/>
+			))}
+		</div>
+	)
+}
+
 const Feed = () => {
   const [ searchText, setSearchText ] = useState('');
+  const [ posts, setPosts ] = useState([]);
 
   const handleSearchChange = (e) => {
 
   }
+  
+  // fetching posts from API //
+  useEffect (() => {
+	const fetchPosts = async () => {
+		const response = await fetch('/api/prompt');
+		const data = await response.json();
+
+		setPosts(data);
+	}
+	fetchPosts();
+  }, []);
 
   return (
 	<section className='feed'>
@@ -15,6 +40,11 @@ const Feed = () => {
 			<input type='text' placeholder='Search for a tag or username' value={searchText} onChange={handleSearchChange} required className='search_input peer'>
 			</input>
 		</form>
+		<PromptCardList 
+
+		data={posts}
+		handleTagClick={() => {}}
+		/>
 	</section>
   )
 }
