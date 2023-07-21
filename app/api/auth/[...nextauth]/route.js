dac9d3849fe2b7a206cdad96183965e1626d98d5
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-20 23:47:41 -0300

    added Google auth to MongoDB database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,38 +11,50 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
 
-    session.user.id = sessionUser._id.toString();
+  callbacks: {
+    async session({ session, user }) {
+      try {
+        if (!user) return session; // No user, nothing to do
 
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      // first, it will check if the user exists
-      const userExists = await User.findOne({
-        email: profile.email,
-      });
-
-      // if it does not, it will create it
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
+        await connectToDB();
+        const sessionUser = await User.findOne({
+          email: user.email,
         });
-      }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        if (sessionUser) {
+          session.user.id = sessionUser._id.toString();
+        }
+
+        return session;
+      } catch (error) {
+        console.log("Error in session callback:", error);
+        return session;
+      }
+    },
+
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log("Error in signIn callback:", error);
+        return false;
+      }
+    },
   },
 });
 
