ff88d994e357a759e70d93d215e23d1c4619ec1a
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-21 03:08:33 -0300

    fixed error with nextRouter

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,11 +1,14 @@
 "use client";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
