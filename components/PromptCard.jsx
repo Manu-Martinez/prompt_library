6d68119951e7a0a03b8356a4ffca6005fe2ee945
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-22 02:05:55 -0300

    fixed issue with creator Id

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -68,8 +68,8 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         {" "}
         {post.tag}{" "}
       </p>
-      {session?.user.id === post.creator._id && pathName === '/profile' && (
-		<div>
+      {session?.user.id === post.creator?._id && pathName === '/profile' && (
+		<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
 			<p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
 				Edit
 			</p>
