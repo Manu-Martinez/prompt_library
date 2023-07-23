7a2c608ed8d6d59a5179dc4d7ff12c4d17f51be0
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-23 00:05:47 -0300

    deleted unused import

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-import Provider from "./Provider";
 
 const Nav = () => {
   const { data: session } = useSession();
