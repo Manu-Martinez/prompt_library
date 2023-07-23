94dc1352144f06f9785e03aaf5c32e1d7705b735
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-23 00:07:38 -0300

    fixed a typo

--- components/Form.jsx
+++ components/Form.jsx
@@ -53,7 +53,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             disabled={submitting}
             className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
           >
-            {submitting ? `${type}ing...` : type}
+            {submitting ? `Creating...` : type}
           </button>
         </div>
       </form>
