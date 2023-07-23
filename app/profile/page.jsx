cfb8d143db83c004c8721ca1856be79947cc0e49
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-23 00:06:36 -0300

    added handleEdit and handleDelete functions to the Profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,9 +7,30 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
   const { data: session } = useSession();
-
   const [posts, setPosts] = useState([]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+	const userConfirmation = confirm('Are you sure you want to delete this prompt?');
+
+	if(userConfirmation) {
+		try {
+			await fetch(`/api/prompt/${post._id.toString()}`, {
+				method: 'DELETE'
+			});
+
+			const filteredPosts = posts.filter((p) => p._id !== post._id);
+			setPosts(filteredPosts);
+		} catch (error) {
+			console.log('error');
+		}
+	}
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -21,8 +42,6 @@ const MyProfile = () => {
     if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
   return (
     <Profile
       name="My"
