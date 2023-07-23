acae2dd986f9e9958d9c8db5150306b24643ad5b
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-23 00:08:24 -0300

    fixed issue with handleEdit and handleDelete buttons

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,8 +6,6 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-
-
   const [copied, setCopied] = useState("");
   const defaultUserImage = "/default-user-image.png";
   const { data: session } = useSession();
@@ -68,16 +66,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         {" "}
         {post.tag}{" "}
       </p>
-      {session?.user.id === post.creator?._id && pathName === '/profile' && (
-		<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-			<p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
-				Edit
-			</p>
-			<p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>
-				Delete
-			</p>
-		</div>
-	  )}
+      {session?.user.id === post.creator?._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
