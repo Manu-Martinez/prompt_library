c10292b4cb00907237182786ac8245536a6cb2d1
Manu-Martinez <manu.martinezgalan@gmail.com>
2023-07-23 17:37:33 -0300

    added show other users profiles

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,77 +5,84 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
-  const [copied, setCopied] = useState("");
-  const defaultUserImage = "/default-user-image.png";
+const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const { data: session } = useSession();
   const pathName = usePathname();
   const router = useRouter();
 
+  const [copied, setCopied] = useState("");
+
+  const handleProfileClick = () => {
+    console.log(post);
+
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+  };
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(() => setCopied("", 3000));
+    setTimeout(() => setCopied(false), 3000);
   };
 
-  // Check if post.creator and post.creator.image exist before rendering the Image component
-  const imageSrc = post?.creator?.image || defaultUserImage;
-
   return (
-    <div className="prompt_card">
-      <div className="flex justify-between items-start gap-5">
-        <div>
-          {/* Use conditional rendering to handle the image */}
-          {post?.creator?.image ? (
-            <Image
-              src={imageSrc}
-              alt="user_image"
-              width={40}
-              height={40}
-              className="rounded-full object-contain"
-            />
-          ) : (
-            <span>No image available</span>
-          )}
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div
+          className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+          onClick={handleProfileClick}
+        >
+          <Image
+            src={post.creator?.image}
+            alt='user_image'
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
+          />
+
+          <div className='flex flex-col'>
+            <h3 className='font-satoshi font-semibold text-gray-900'>
+              {post.creator?.username}
+            </h3>
+            <p className='font-inter text-sm text-gray-500'>
+              {post.creator?.email}
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col">
-          <h3 className="font-satoshi font-semibold text-gray-900">
-            {post.creator?.username}
-          </h3>
-          <p className="font-inter text-sm text-gray-500">
-            {post.creator?.email}{" "}
-          </p>
-        </div>
-        <div className="copy_btn" onClick={handleCopy}>
+
+        <div className='copy_btn' onClick={handleCopy}>
           <Image
             src={
               copied === post.prompt
-                ? "assets/icons/tick.svg"
-                : "assets/icons/copy.svg"
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
             }
+            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
         </div>
       </div>
-      <p className="my-4 font-satoshi text-sm text-gray-700"> {post.prompt}</p>
+
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
       <p
-        className="font-inter text-sm blue_gradient cursor-pointer"
-        onClick={() => handleClick && handleTagClick(post.tag)}
+        className='font-inter text-sm blue_gradient cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {" "}
-        {post.tag}{" "}
+        #{post.tag}
       </p>
+
       {session?.user.id === post.creator?._id && pathName === "/profile" && (
-        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
           <p
-            className="font-inter text-sm green_gradient cursor-pointer"
+            className='font-inter text-sm green_gradient cursor-pointer'
             onClick={handleEdit}
           >
             Edit
           </p>
           <p
-            className="font-inter text-sm orange_gradient cursor-pointer"
+            className='font-inter text-sm orange_gradient cursor-pointer'
             onClick={handleDelete}
           >
             Delete
